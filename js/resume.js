68bfaa54e73d933ad6cc1ded4de249a1208806c8
pareen89 <pareenpatel89@gmail.com>
2019-03-04 09:38:17 -0800

    Cleaned up js code to make more readable and reusable

--- js/resume.js
+++
@@ -1,7 +0,0 @@
-let minResumeFile = document.getElementById('minResumeFile');
-let closeResumeFile = document.getElementById('closeResumeFile');
-let resume = document.getElementById('resume');
-minResumeFile.addEventListener('click', function () {
-    resume.style.display = "none";
-    clickedFile.style.backgroundColor = "transparent";
-})
